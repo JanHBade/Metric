0669f05e6194e592251697e082aae16c05f47b32
JHB <github@1wiki.de>
2024-11-07 10:07:44 +0100

    add conuter

--- EnvToMqtt.py
+++ EnvToMqtt.py
@@ -14,15 +14,28 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
 from paho.mqtt import client as mqtt_client
 client = mqtt_client.Client()
 
+Zaehler = 0
+
 if __name__ == '__main__':	
 	client.connect("Isis")
 
 	while True:
+		client.reconnect()
+
 		data = {}
+		data["Zaehler"]=Zaehler + 1
 		data["Temperatur"]=bme280.temperature
 		data["Druck"]=bme280.pressure
 		data["Feuchte"]=bme280.humidity
 
-		client.publish("SPS/Dachboden",json.dumps(data))
+		try:
+			client.publish("SPS/Dachboden",json.dumps(data))
+		except:
+			print("Error: pub")
 
-		time.sleep(59)
+		i = 0
+		while i < 60:
+			client.loop()
+			time.sleep(0.2)
+			i += 1
+			#print(i)
